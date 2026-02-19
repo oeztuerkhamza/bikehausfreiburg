84bc7cd95ab3e795f298cb8b2e7d558e4c45b31c
oeztuerkhamza <oeztuerk.hamza@web.de>
2026-02-19 01:02:44 +0100

    Update API URL and CORS for production

--- BikeHaus.Client/src/environments/environment.prod.ts
+++ BikeHaus.Client/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Render backend URL - will be updated after deployment
-  apiUrl: 'https://bikehaus-api.onrender.com/api',
+  apiUrl: 'https://bikehaus-api-seod.onrender.com/api',
 };
