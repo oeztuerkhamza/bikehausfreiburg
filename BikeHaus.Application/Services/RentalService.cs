a5e0cf88b4e2afe432dd50c964f49a2a189490a4
Hamza Öztürk <oeztuerk.hamza@web.de>
2026-05-21 18:45:19 +0200

    FIX  bike rent

--- BikeHaus.Application/Services/RentalService.cs
+++ BikeHaus.Application/Services/RentalService.cs
@@ -104,14 +104,6 @@ public class RentalService : IRentalService
         {
             var bicycle = await _bicycleRepository.GetByIdAsync(bikeDto.BicycleId)
                 ?? throw new KeyNotFoundException($"Fahrrad mit ID {bikeDto.BicycleId} nicht gefunden.");
-
-            var existingRental = await _rentalRepository.GetActiveByBicycleIdAsync(bikeDto.BicycleId);
-            if (bicycle.Status != BikeStatus.Available &&  existingRental.CustomerId != dto.Customer.Id)
-                throw new InvalidOperationException($"Fahrrad '{bicycle.Marke} {bicycle.Modell}' ist nicht verfügbar für Vermietung.");
-
-            if (existingRental != null && existingRental.CustomerId != dto.Customer.Id)
-                throw new InvalidOperationException($"Fahrrad '{bicycle.Marke} {bicycle.Modell}' ist bereits vermietet.");
-
             bicycles.Add(bicycle);
         }
 
